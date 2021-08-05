37dd50108172749ce472a1be5f7bc3b0f8ee7d06
mankojanos <mankojanos90@hotmail.com>
2021-08-05 14:40:05 +0200

    homepage layout

--- instagram/resources/views/home.blade.php
+++ instagram/resources/views/home.blade.php
@@ -6,14 +6,31 @@
         <div class="col-3 p-5">
             <img src="png/user.png">
         </div>
-        <div class="col-9">
+        <div class="col-9 pt-5">
             <div><h1>ExampleUserName</h1></dv>
-            <div>
-                <div></div>
-                <div></div>
-                <div></div>
+            <div class="d-flex">
+                <div class="pr-4"><strong>51</strong> posts</div>
+                <div class="pr-4"><strong>119</strong> folowers</div>
+                <div class="pr-4"><strong>212</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">Lorem ipsum</div>
+            <div>Nunc libero felis, vehicula in metus quis, eleifend semper nibh. Nullam aliquet quis erat in auctor.</div>
+            <div><a href="#">github.com/mankojanos</a></div>
             </div>
         </div>
     </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://dummyimage.com/300" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://dummyimage.com/300" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://dummyimage.com/300" class="w-100">
+        </div>
+
+    </div>
+
 </div>
 @endsection
