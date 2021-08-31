0ac1a7c433515216e47c4729bdea99d7c1a80d7e
mankojanos <mankojanos90@hotmail.com>
2021-08-31 13:04:58 +0200

    many to many releation part 2

--- instagram/app/Http/Controllers/ProfilesController.php
+++ instagram/app/Http/Controllers/ProfilesController.php
@@ -11,8 +11,8 @@ class ProfilesController extends Controller
 {
     public function index(User $user)
     {
-        $follows = (auth()->user()) ? auth()->user()->following->contains($user) : false;
-        return view('profiles.index', compact('user', $follows));
+        $follows = (auth()->user()) ? auth()->user()->following->contains($user->id) : false;
+        return view('profiles.index', compact('user', 'follows'));
     }
 
     public function edit(User $user)
--- instagram/resources/js/components/FollowButton.vue
+++ instagram/resources/js/components/FollowButton.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
-        <button class="btn btn-primary ml-4" @click="followUser">Follow</button>
+        <button class="btn btn-primary ml-4" @click="followUser" v-text="buttonText"></button>
     </div>   
 </template>
 
 <script>
     export default {
-        props: ['userId'],
+        props: ['userId', 'follows'],
 
         mounted() {
             console.log('Component mounted.')
@@ -14,18 +14,31 @@
 
         data: function() {
             return {
-                status:
+                status: this.follows,
             }
-        }
+        },
 
         methods: {
             followUser()
             {
                 axios.post('/follow/' + this.userId)
                     .then(response => {
+                        this.status = ! this.status;
+
                         console.log(response.data);
+                    })
+                    .catch(errors => {
+                        if (errors.response.status == 401) {
+                            window.location = '/login';
+                        }
                     });
             }
-        }
+        },
+
+        computed: {
+            buttonText() {
+                return (this.status) ? 'Unfollow' : 'Follow';
+            }
+        }        
     }
 </script>
