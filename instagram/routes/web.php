49e62427abc73ca2eb728661e0e151168e599f23
mankojanos <mankojanos90@hotmail.com>
2021-09-02 09:03:57 +0200

    redirect, email

--- instagram/routes/web.php
+++ instagram/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Mail\NewUserWelcomeMail;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,18 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', '');
-
 Auth::routes();
 
-Route::post('follow/{user}', [App\Http\Controllers\FollowsController::class, 'store']);
+Route::get('/email', function() {
+    return new NewUserWelcomeMail();
+});
+
+Route::post('/follow/{user}', [App\Http\Controllers\FollowsController::class, 'store']);
 
 Route::get('/', [App\Http\Controllers\PostsController::class, 'index']);
 Route::get('/p/create', [App\Http\Controllers\PostsController::class, 'create']);
-Route::get('/p/{post}', [App\Http\Controllers\PostsController::class, 'show']);
 Route::post('/p', [App\Http\Controllers\PostsController::class, 'store']);
-
+Route::get('/p/{post}', [App\Http\Controllers\PostsController::class, 'show']);
 
 Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
 Route::get('/profile/{user}/edit', [App\Http\Controllers\ProfilesController::class, 'edit'])->name('profile.edit');
-Route::patch('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'update'])->name('profile.edit');
+Route::patch('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'update'])->name('profile.update');
